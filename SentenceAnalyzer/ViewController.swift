c7eafecd345f160f3954c7fdd5971f2ad3bcd104
jeonyul <jeonyul00@gmail.com>
2024-11-06 19:25:27 +0900

    first commit

---
+++ SentenceAnalyzer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SentenceAnalyzer
+//
+//  Created by 전율 on 11/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
